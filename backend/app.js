6b3fc57801dd63c3f1b384acf42a5ca51adae2a2
Nyttho <anthony.dms@outlook.com>
2025-02-09 12:00:23 +0100

    minimal server setup

--- backend/app.js
+++ backend/app.js
@@ -4,3 +4,10 @@ import express from "express";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+//parse req body
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("listening on port", PORT);
+});
