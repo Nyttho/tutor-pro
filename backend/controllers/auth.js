fa3670a3ef1e8bcfb3df200485c2e768a419e051
Nyttho <anthony.dms@outlook.com>
2025-02-12 18:46:21 +0100

    authentication controller created

---
+++ backend/controllers/auth.js
@@ -0,0 +1,44 @@
+import User from "../models/User.js";
+import bcryptjs from "bcryptjs";
+import { generateAccessToken } from "../utils/tokens.js";
+
+const auth = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //check if user in bdd
+    const user = await User.getByEmail(email);
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    //check password
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    const accessToken = generateAccessToken(user);
+
+    //cookie
+    const isProduction = process.env.NODE_ENV === "production";
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: isProduction ? "strict" : "lax",
+      maxAge: 1000 * 60 * 15,
+    });
+
+    const { passwordb, ...cleanedUser } = user;
+
+    return res
+      .status(200)
+      .json({ message: "Authentication successfull", user: cleanedUser });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+const authController = { auth };
+
+export default authController;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import authController from "../controllers/auth.js";
 
 const authRouter = Router();
+
+authRouter.post("/login", authController.auth);
 
 export default authRouter;
