0e63f5c352998e63d5628bdcb77a3a4b86b25ca9
Anthony DUMAS <a.dumas@baclesse.unicancer.fr>
2025-02-13 13:06:12 +0100

    get /api/category/:id route created

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -15,8 +15,24 @@ const getAllCategories = async (req, res) => {
     }
 }
 
+const getOneCategory = async (req, res) => {
+    try {
+        const categoryId = req.params.id
+        const category = await Category.getById(categoryId)
 
-const categoryController = {getAllCategories}
+        if(!category) {
+            return res.status(404).json({error: "Category not found"})
+        }
+
+        return res.status(200).json({category})
+
+    } catch (err) {
+        return res.status(500).json({error: err.message})
+    }
+}
+
+
+const categoryController = {getAllCategories, getOneCategory}
 
 export default categoryController;
 
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -5,5 +5,7 @@ import isAuth from "../middlewares/isAuth.js";
 const categoryRouter = Router();
 
 categoryRouter.get("/", categoryController.getAllCategories);
+categoryRouter.get("/:id", categoryController.getOneCategory);
+
 
 export default categoryRouter;
