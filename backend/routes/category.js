0be3a0b933ff1c3e21bc16b1485ef56f75eec922
Anthony DUMAS <a.dumas@baclesse.unicancer.fr>
2025-02-13 14:32:08 +0100

    swagger doc for get /api/category

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -5,6 +5,53 @@ import isAdmin from "../middlewares/isAdmin.js";
 
 const categoryRouter = Router();
 
+
+/**
+ * @swagger
+ * /categories:
+ *   get:
+ *     summary: Retrieve all categories
+ *     description: Fetches a list of all available categories.
+ *     tags:
+ *       - Categories
+ *     responses:
+ *       200:
+ *         description: List of categories retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: "Mathematics"
+ *       404:
+ *         description: No category found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: No category found
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Internal server error
+ */
+
 categoryRouter.get("/", categoryController.getAllCategories);
 categoryRouter.get("/:id", categoryController.getOneCategory);
 categoryRouter.post("/", isAuth, isAdmin, categoryController.createCategory);
