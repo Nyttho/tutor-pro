715f1ecd2bcda63f6f5168c4e83f1559fea467ea
Nyttho <anthony.dms@outlook.com>
2025-02-17 11:47:34 +0100

    getBySubject method for lesson

--- backend/models/Lesson.js
+++ backend/models/Lesson.js
@@ -5,6 +5,15 @@ class Lesson extends Crud {
   constructor() {
     super("lessons");
   }
+  async getBySubject(subjectId, userId) {
+    try {
+      const query = `SELECT * FROM lessons WHERE subject_id = $1 AND user_id = $2;`;
+      const result = await pool.query(query, [subjectId, userId]);
+      return result.rows[0] || null;
+    } catch (err) {
+      return { error: "Error while checking for lessons" };
+    }
+  }
 }
 
 export default new Lesson();
