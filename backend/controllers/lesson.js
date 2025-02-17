5344de9f53da3dcb597642134116ca211ebd747f
Nyttho <anthony.dms@outlook.com>
2025-02-17 10:18:11 +0100

    get /api/lesson/ route created

--- backend/controllers/lesson.js
+++ backend/controllers/lesson.js
@@ -1,7 +1,17 @@
 import Lesson from "../models/Lesson.js";
 
-const getAllLessons = (req, res) => {
+const getAllLessons = async (req, res) => {
   try {
+    const userId = parseInt(req.user.id);
+
+    const lessons = await Lesson.getAll().filter(
+      (l) => l.created_by === userId
+    );
+
+    if (lessons.length === 0) {
+      return res.status(404).json({ error: "Lessons not found" });
+    }
+    return res.status(200).json(lessons);
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
