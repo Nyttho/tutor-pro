458d6264e9ab09b000b5f5656694fc5a18067774
Nyttho <anthony.dms@outlook.com>
2025-03-12 18:44:34 +0100

    get stats

--- frontend/src/utils/getStats.ts
+++ frontend/src/utils/getStats.ts
@@ -1,30 +1,33 @@
-import { Student } from "../types/StudentType"
+import { Student } from "../types/StudentType";
 
-const backendUrl = import.meta.env.VITE_BACKEND
+const backendUrl = import.meta.env.VITE_BACKEND;
 
 export const getStudentsNb = async () => {
-    try {
-        const response = await fetch(`${backendUrl}/api/student`, {
-            credentials: 'include'
-        })
-        const datas = await response.json()
-        return datas.filter((student: Student) => student?.isActive).length
-    } catch(err){
-        console.error("Error fetching students")
-    }
-}
+  try {
+    const response = await fetch(`${backendUrl}/api/student`, {
+      credentials: "include",
+    });
+    const datas = await response.json();
+    return datas.filter((student: Student) => student?.isActive).length;
+  } catch (err) {
+    console.error("Error fetching students");
+  }
+};
 
 export const getCourses = async () => {
-    try {
-        const today = new Date()
-        const year = today.getFullYear()
-        const month = today.getMonth() + 1
-        const response = await fetch(`${backendUrl}/api/course?year=${year}&month=${month}`, {
-             credentials: 'include'
-        })
-        const datas = await response.json()
-        return datas
-    } catch (err) {
-        console.error("Error fetching courses")
-    }
-}
+  try {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const response = await fetch(
+      `${backendUrl}/api/course?year=${year}&month=${month}`,
+      {
+        credentials: "include",
+      }
+    );
+    const datas = await response.json();
+    return datas;
+  } catch (err) {
+    console.error("Error fetching courses");
+  }
+};
